65d53451668d038d21e252882d920e72e747e7cc
shadH57 <shadbaram57@gmail.com>
2026-07-20 00:42:26 +0300

    Add error handling middleware to manage application errors

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import customerRoute from './routes/customer.route.js'
 import orderRoute from './routes/order.route.js'
 import tableRoute from './routes/table.route.js'
 import menuRoute from './routes/menu.route.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 
 dotenv.config();
@@ -15,6 +16,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Middleware
 app.use(cors({
     origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
     credentials: true,
@@ -22,11 +24,15 @@ app.use(cors({
 
 app.use(express.json());
 
+//Routes
 app.use('api/user', userRoute)
 app.use('/api/customer', customerRoute)
 app.use('/api/order', orderRoute)
 app.use('/api/tables', tableRoute)
 app.use('/api/menu', menuRoute)
+
+// Error Handling Middleware
+app.use(errorHandler)
 
 
 connectDB().then(() => {
